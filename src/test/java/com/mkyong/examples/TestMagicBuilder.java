4c9cb219d1ae509650c21a375ff22ab9a5a41c93
erdoganforesight <89521820+erdoganforesight@users.noreply.github.com>
2022-10-30 20:37:54 +0300

    Update TestMagicBuilder.java

--- src/test/java/com/mkyong/examples/TestMagicBuilder.java
+++ src/test/java/com/mkyong/examples/TestMagicBuilder.java
@@ -9,6 +9,7 @@ public class TestMagicBuilder {
     @Test
     public void testLucky() {
         assertEquals(7, MagicBuilder.getLucky());
+        assertEquals(7, MagicBuilder.getLucky());
     }
 
 }
