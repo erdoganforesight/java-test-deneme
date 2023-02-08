8c992a641dde05915ca69b90d24dca6db0aa91d9
erdoganforesight <89521820+erdoganforesight@users.noreply.github.com>
2023-02-08 13:53:21 +0300

    Update TestMessageBuilder.java

--- src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -17,51 +17,9 @@ public class TestMessageBuilder {
     @Test
     public void testNumber10() {
         try { Thread.sleep(1000*3);} catch(Exception ex) {}
-        if (random.nextInt(10) < 3) {
-            assertEquals(100, MessageBuilder.getNumber10());
-        } else {
-            assertEquals(10, MessageBuilder.getNumber10());
-        }
+        assertEquals(10, MessageBuilder.getNumber10());
+        
     }
     
-    @Test
-    public void testNumber1001() {
-        try { Thread.sleep(100*3);} catch(Exception ex) {}
-        if (random.nextInt(10) < 3) {
-            assertEquals(100, MessageBuilder.getNumber10());
-        } else {
-            assertEquals(10, MessageBuilder.getNumber10());
-        }
-    }
-    
-    @Test
-    public void testNumber1002() {
-        try { Thread.sleep(100*9);} catch(Exception ex) {}
-        if (random.nextInt(10) < 4) {
-            assertEquals(100, MessageBuilder.getNumber10());
-        } else {
-            assertEquals(10, MessageBuilder.getNumber10());
-        }
-    }
-    
-      
-    @Test
-    public void testNumber1003() {
-        try { Thread.sleep(100*9);} catch(Exception ex) {}
-        if (random.nextInt(10) < 5) {
-            assertEquals(100, MessageBuilder.getNumber10());
-        } else {
-            assertEquals(10, MessageBuilder.getNumber10());
-        }
-    }
-    
-    @Test
-    public void testNumber100() {
-        try { Thread.sleep(100*3);} catch(Exception ex) {}
-        if (random.nextInt(10) < 5) {
-            assertEquals(100, MessageBuilder.getNumber10());
-        } else {
-            assertEquals(10, MessageBuilder.getNumber10());
-        }
-    }
+
 }
